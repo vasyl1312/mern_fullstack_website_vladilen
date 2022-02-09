be18d936cf3b7e2398f9de91aaefd55aa26f0f7e
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-09 14:24:30 +0200

    create server, connect to mongodb

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+const PORT = config.get('port') || 5000;
+async function start(req, res, next) {
+    try {
+        await mongoose.connect(config.get('mongoUri'), { 
+            
+        })
+        app.listen(PORT, ()=>{
+            console.log(`App has been listened on port ${PORT}...`);
+        })
+    }catch(err) {
+        console.log("Server ERROR", err);
+        process.exit(1);
+    }
+}
+start();
+
