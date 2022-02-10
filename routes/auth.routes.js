b9b628fc2aa8801ba365c8357b5120489dd82dc9
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-10 18:00:34 +0200

    login and some checks

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router =  Router();
@@ -15,7 +17,7 @@ router.post(
  try {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array(), message:'Uncorrected input...'})
+        return res.status(400).json({ errors: errors.array(), message:'Uncorrected input for registration...'})
     }
 
     const {email, password} = req.body
@@ -35,8 +37,39 @@ router.post(
 })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+    '/login',
+    [
+        check('email', 'Input correct email').normalizeEmail().isEmail(),
+        check('password', 'Input password').exists()
+    ],
+    async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array(), message:'Uncorrected input for login...'})
+        }
+        const {email, password} = req.body;
+        const user = await User.findOne({ email})
+        if(!user){
+            return res.status(400).json({message: 'Account not found, try again...'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message: 'Password is not correct, try again...'})
+        }
+        //якщо всі перевірки пройшли то робимо авторизацію
 
+        const token = jwt.sign(
+            {userId: user.id},
+            config.get('jwtSecret'),
+            {expiresIn: '1h'}
+        )
+        res.json({token, userId: user.id})
+
+     }catch (e) {
+         res.status(500).json({message: 'Error, try again!'});
+     }
 })
 
 module.exports = router
