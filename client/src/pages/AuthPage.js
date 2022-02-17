9cb234912a165cc7bec1d8ece33addfe1b1903b4
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-17 20:43:59 +0200

    work with request

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
+import {useHttp} from '../hooks/http.hook'
 
 export const AuthPage = () => {
+    const {loading, request} = useHttp()
     const [form, setForm] = useState({
         email:"", password:""
     })
 
     const changeHandler = event => {
         setForm({...form, [event.target.value]:event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            console.log('Data', data)
+        }catch (e) {}
     }
 
     return (
@@ -45,8 +54,20 @@ export const AuthPage = () => {
                   </div>
                 </div>
                  <div className="card-action">
-                    <button className="btn yellow darken-4" style={{marginRight: 10}}>Увійти</button>
-                    <button className="btn grey lighten-1 black-text">Реєстрація</button>
+                    <button 
+                    className="btn yellow darken-4" 
+                    style={{marginRight: 10}}
+                    disabled={loading}
+                    >
+                        Увійти
+                        </button>
+                    <button 
+                    className="btn grey lighten-1 black-text"
+                    onClick={registerHandler}
+                    disabled={loading}
+                    >
+                        Реєстрація
+                        </button>
 
                 </div>
             </div>
